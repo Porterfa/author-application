8dfb1a1b3bf1641ab9df517ecc753f2078b1d54f
Porterfa <Porterfa@tcd.ie>
2022-10-10 23:11:07 +0100

    final changes

--- src/Components/Home/Socials.js
+++ src/Components/Home/Socials.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Socials.css';
+import insta from './insta.png';
+import emails from './email.png';
+import amazon from './amazon.png';
 
 function Socials() {
     return (
@@ -9,17 +12,17 @@ function Socials() {
                     <tr>
                     <td>
                         <a target="_blank" href='https://www.instagram.com/thesuzanneaugust'>
-                            <i className="fa-brands fa-instagram fa-3x"></i>
+                            <img src={insta} className="image__insta"/>
                         </a>
                     </td>
                     <td>
                         <a target="_blank" href='https://mailchi.mp/314c50f4f1bf/join-my-email-list'>
-                            <i className="fa-regular fa-envelope fa-3x"></i>
+                            <img src={emails} className="image__emails"/>
                         </a>
                     </td>
                     <td>
                         <a target="_blank" href='https://www.amazon.com/Suzanne-August/e/B09B7VSVDZ?ref=dbs_a_def_rwt_sims_vu00_r13_c1'>
-                            <i className="fa-brands fa-amazon fa-3x"></i>
+                            <img src={amazon} className="image__amazon"/>
                         </a>
                     </td>
                     </tr>
